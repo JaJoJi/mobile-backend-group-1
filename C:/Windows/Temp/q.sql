38d79fdc23c614382baaf32270fcf45f0ed5faaa
JaJoJi <161248189+JaJoJi@users.noreply.github.com>
2026-08-31 10:44:47 +0700

    Delete C:/Windows/Temp directory

--- C:/Windows/Temp/q.sql
+++
@@ -1,3 +0,0 @@
-SELECT "productId", "remainingStock", "availableStock" FROM products WHERE "productId"='p-1001';
-SELECT COUNT(*) FROM orders WHERE "productId"='p-1001' AND status='SUCCESS';
-SELECT "userId" FROM orders WHERE "productId"='p-1001' AND status='SUCCESS' ORDER BY "createdAt" ASC LIMIT 30;
